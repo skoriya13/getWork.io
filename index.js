a2d724cacb7bd396f1991460ccb61fe31ecc63d2
virenribadiya <vribadiya278@rku.ac.in>
2023-02-04 01:44:18 +0530

    server created

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.listen(5000,function(){
+    console.log("server is up @ http://localhost:5000");
+})
